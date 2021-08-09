deb8dab455e90a2276d5b51d5e294a95e1e3bc2c
balqis intan <balqis.icl@gmail.com>
2021-08-09 11:21:36 +0700

    day 1 pelatihan

---
+++ mainapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def landing_page(request):
+    a = 3
+    b = 5
+    c = a+b
+    return HttpResponse(a+b)
+
+def second_page(request):
+    return HttpResponse("SecondPage")
+
+def example(request):
+    return render(request, 'example.html')
